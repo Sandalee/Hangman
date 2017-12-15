684a0fcac4669d4e8b1ecc2ffee69fc6312872b3
San16 <sandalee.bandara@gmail.com>
2017-12-15 10:18:54 +0530

    Generate random index & a word based on the index

---
+++ app.js
@@ -0,0 +1,31 @@
+var app=angular.module("HangmanApp",[]);
+app.controller("GameController",['$scope',function($scope){
+	
+	var words=["cat","rat","bat","mat"];
+	$scope.incorrectLettersChosen=[];
+	$scope.correctLettersChosen=[];
+	$scope.guesses=6;
+	$scope.displayWord='';
+	$scope.input={
+		letter:''
+	}
+
+	var selectRandomWord= function(){
+		var index= Math.round(Math.random()*words.length);
+		return words[index];
+	}
+
+	var newGame = function(){
+
+		$scope.incorrectLettersChosen=[];
+		$scope.correctLettersChosen=[];
+		$scope.guesses=6;
+		$scope.displayWord='';
+
+		selectedWord = selectRandomWord();
+		console.log(selectedWord);
+
+	}
+
+	newGame();
+}]);

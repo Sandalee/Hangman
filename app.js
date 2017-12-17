fcfe77e3557ab0e37f68018079f31ac542effb2f
Sandalee16 <sandalee.bandara@gmail.com>
2017-12-17 20:15:47 +0530

    Game complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app=angular.module("HangmanApp",[]);
-app.controller("GameController",['$scope',function($scope){
+app.controller("GameController",['$scope','$timeout',function($scope,$timeout){
 	
 	var words=["cat","rat","bat","mat"];
 	$scope.incorrectLettersChosen=[];
@@ -23,7 +23,7 @@ app.controller("GameController",['$scope',function($scope){
 		$scope.displayWord='';
 
 		selectedWord = selectRandomWord();
-		//console.log(selectedWord);
+		console.log(selectedWord);
 		//hide lettersfor the user
 		var tempDisplayWord = '';
 		for (var i = 0; i < selectedWord.length; i++) {
@@ -38,7 +38,7 @@ app.controller("GameController",['$scope',function($scope){
 
 	$scope.letterChosen = function(){
 		//console.log("working!"); 
-		for (var i = 0; i < $scope.correctLettersChosen.length; i++{
+		for (var i = 0; i < $scope.correctLettersChosen.length; i++){
 			if($scope.correctLettersChosen[i].toLowerCase()==$scope.input.letter.toLowerCase())
 			{
 				$scope.input.letter=""; //clear the input field
@@ -46,13 +46,52 @@ app.controller("GameController",['$scope',function($scope){
 			}			
 		}
 
-		for (var i = 0; i < $scope.incorrectLettersChosen.length; i++{
+		for (var i = 0; i < $scope.incorrectLettersChosen.length; i++){
 			if($scope.incorrectLettersChosen[i].toLowerCase()==$scope.input.letter.toLowerCase())
 			{
 				$scope.input.letter=""; //clear the input field
 				return;
 			}			
 		}
+
+		var correct = false;
+		for (var i = 0; i < selectedWord.length; i++) 
+		{
+			if(selectedWord[i].toLowerCase()==$scope.input.letter.toLowerCase())
+			{
+				//to display the word with correct guesses
+				$scope.displayWord = $scope.displayWord.slice(0,i)+$scope.input.letter.toLowerCase()+$scope.displayWord.slice(i+1);
+				correct = true;
+
+			}	
+		}
+
+		if(correct){
+			$scope.correctLettersChosen.push($scope.input.letter.toLowerCase());
+		}
+		else{
+			$scope.guesses--;
+			$scope.incorrectLettersChosen.push($scope.input.letter.toLowerCase());
+		}
+		$scope.input.letter = "";
+		if($scope.guesses == 0)
+		{
+			//alert("YOU LOST!");
+			$timeout(function(){
+				newGame();
+			},500);
+
+		}
+
+		//user has guessed right
+		if($scope.displayWord.indexOf("*") == -1)
+		{
+			//alert("Awesome! You Won!");
+			$timeout(function(){
+				newGame();
+			},500);
+		}
+
 
 	}
 
